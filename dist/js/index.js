1e596939cf265f4e8526ae2828f1f5e9fe5ef8ae
Alexander <aleksander.freelancer@gmail.com>
2023-02-16 06:35:24 +0300

    Finished third section

--- dist/js/index.js
+++ dist/js/index.js
@@ -3,6 +3,21 @@ let button_gamburger = document.querySelector(".nav-bar__gamburger");
 let checkbox = document.querySelector(".nav-bar__checkbox");
 let nav_menu = document.querySelector(".nav-menu");
 let nav_list = document.querySelector(".nav-list");
+let figures = document.querySelector(".figures");
+let cofficent = 0
+if(window.innerWidth <= 850) {
+    cofficent = 0.7;
+}
+if(window.innerWidth <= 450) {
+    cofficent = 0.5;
+}
+if(window.innerWidth > 850){
+    cofficent = 1
+}
+for (const element of figures.children) {
+    element.style.width = element.clientWidth * cofficent + "px";
+    element.style.height = element.clientHeight * cofficent + "px";
+}
 let nav_menu_list = nav_menu.firstChild
 // Create elements 
 let list_element = nav_list.firstElementChild.cloneNode(true)
@@ -13,27 +28,12 @@ for (const element of nav_list.children) {
     nav_menu_list.appendChild(list_element)
 }
 button_gamburger.addEventListener("click", () => {
-    if(checkbox.checked == true){
+    if (checkbox.checked == true) {
         nav_menu.style.width = "100%";
         nav_menu.style.opacity = "1";
-    } 
-    else{
-        nav_menu.style.opacity = "0";
-        nav_menu.style.width = "0%"; 
-    } 
-})
-// Define function that implement the animation
-function animation(){
-    nav_menu.style.opacity = "1";
-    nav_menu.style.width = "100%";
-}
-// Write counter with recursion
-function counter(){
-    if(nav_menu.style.opacity == "1"){
+    }
+    else {
         nav_menu.style.opacity = "0";
         nav_menu.style.width = "0%";
-        setTimeout(() => {
-            counter()
-        }, 1000);
     }
-}
+})
--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,13 @@
-const gulp        = require('gulp');
+const gulp = require('gulp');
 const browserSync = require('browser-sync');
-const sass        = require('gulp-sass')(require('sass'));
+const sass = require('gulp-sass')(require('sass'));
 const cleanCSS = require('gulp-clean-css');
 const autoprefixer = require('gulp-autoprefixer');
 const rename = require("gulp-rename");
 const imagemin = require('gulp-imagemin');
 const htmlmin = require('gulp-htmlmin');
 
-gulp.task('server', function() {
+gulp.task('server', function () {
 
     browserSync({
         server: {
@@ -16,21 +16,23 @@ gulp.task('server', function() {
     });
 
     gulp.watch("src/*.html").on('change', browserSync.reload);
+    gulp.watch("src/js/**/*.js").on('change', browserSync.reload);
 });
 
-gulp.task('styles', function() {
+gulp.task('styles', function () {
     return gulp.src("src/sass/**/*.+(scss|sass)")
-        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-        .pipe(rename({suffix: '.min', prefix: ''}))
+        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+        .pipe(rename({ suffix: '.min', prefix: '' }))
         .pipe(autoprefixer())
-        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(cleanCSS({ compatibility: 'ie8' }))
         .pipe(gulp.dest("dist/css"))
         .pipe(browserSync.stream());
 });
 
-gulp.task('watch', function() {
+gulp.task('watch', function () {
     gulp.watch("src/sass/**/*.+(scss|sass|css)", gulp.parallel('styles'));
     gulp.watch("src/*.html").on('change', gulp.parallel('html'));
+    gulp.watch("src/js/**/*.js").on('change', gulp.parallel('scripts'));
 });
 
 gulp.task('html', function () {
